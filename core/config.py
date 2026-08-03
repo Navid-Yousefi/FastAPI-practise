fbc6064a2e0273a6a89d94fc15b66640d5d8a73e
Navid Yousife <104007176+NavidYosufie@users.noreply.github.com>
2026-08-03 23:27:46 +0330

    cheange method path url

--- core/config.py
+++ core/config.py
@@ -1,8 +1,9 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URL: str = "sqlite:///./sqlite.db"
+    SQLALCHEMY_DATABASE_URL: str
+    model_config = SettingsConfigDict(env_file='.env')
 
 
 settings = Settings()
--- core/database.py
+++ core/database.py
@@ -5,9 +5,9 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import settings
 
-env_path = Path(__file__).parent.parent / '.env'
-load_dotenv(dotenv_path=env_path)
-DATABASE_URL = os.getenv('DATABASE_URL')
+# env_path = Path(__file__).parent.parent / '.env'
+# load_dotenv(dotenv_path=env_path)
+# DATABASE_URL = os.getenv('DATABASE_URL')
 
 
 engine = create_engine(
